6ac8c2579a55cd6c2ac64e94476e10c8d397e098
Joseph Hirshman <josehirshman@jhengineerartist.com>
2023-07-27 14:24:37 -0700

    made a few changes to the posts

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -7,17 +7,16 @@ export default function BlogPage({ params }: { params: { slug: string } }) {
     const bp: BlogPost = getBlogDataBySlug(params.slug) as BlogPost
     return (
         <main className="bg-enchilada-100">
-            <article className='flex lg:flex-row max-lg:flex-col-reverse justify-center'>
+            <article className='flex lg:flex-row max-lg:flex-col-reverse justify-around'>
                 <div className='mx-auto prose max-lg:prose-base lg:prose-xl md:relative p-6 prose-h3:text-enchilada-600 text-enchilada-600'>
                     <ReactMarkdown>{bp.content}</ReactMarkdown>
                 </div>
-                <figure className='max-lg:top-0 top-20 mx-auto w-1/3 max-h-screen lg:sticky max-lg:h-96 max-lg:w-full relative'>
+                <figure className=' max-lg:top-0 top-20 mx-auto w-2/5 max-h-fit lg:sticky max-lg:h-96 max-lg:w-full relative'>
                     <Image
                         className='relative object-contain object-top'
                         src={bp.heroImage}
                         alt={bp.title}
                         fill />
-                    <figcaption className='relative'>Work Title</figcaption>
                 </figure>
             </article>
         </main >
