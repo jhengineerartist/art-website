2da2a0297e52293e1102512fe06b6826864b550a
Joseph Hirshman <josehirshman@jhengineerartist.com>
2023-07-27 18:20:16 -0700

    fixed up posts css

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -8,12 +8,12 @@ export default function BlogPage({ params }: { params: { slug: string } }) {
     return (
         <main className="bg-enchilada-100">
             <article className='flex lg:flex-row max-lg:flex-col-reverse justify-around'>
-                <div className='mx-auto prose max-lg:prose-base lg:prose-xl md:relative p-6 prose-h3:text-enchilada-600 text-enchilada-600'>
+                <div className='mx-auto prose max-lg:prose-base min-w-1/2 lg:prose-xl md:relative p-6 prose-h3:text-enchilada-600 text-enchilada-600'>
                     <ReactMarkdown>{bp.content}</ReactMarkdown>
                 </div>
-                <figure className=' max-lg:top-0 top-20 mx-auto w-2/5 max-h-fit lg:sticky max-lg:h-96 max-lg:w-full relative'>
+                <figure className='mt-8 items-center mx-auto w-1/2 max-h-fit max-lg:h-96 max-lg:w-full relative'>
                     <Image
-                        className='relative object-contain object-top'
+                        className='object-contain object-top'
                         src={bp.heroImage}
                         alt={bp.title}
                         fill />
