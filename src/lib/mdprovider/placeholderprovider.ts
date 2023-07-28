4aa4df47525a8d4ea77e684f43e259944381b000
Joseph Hirshman <josehirshman@jhengineerartist.com>
2023-07-28 09:22:17 -0700

    Basic fullscreening image implemented

--- src/lib/mdprovider/placeholderprovider.ts
+++ src/lib/mdprovider/placeholderprovider.ts
@@ -4,32 +4,48 @@ import matter from "gray-matter";
 
 const root = process.cwd();
 
-export default function getAllPostsData() {
-  // Get file names under /posts
-  const placeholderPath = path.join(root, "public", "placeholders", "posts");
-  const fileNames = fs.readdirSync(placeholderPath);
-
-  const allPostsData = fileNames.map((file) => {
-    const filePath = path.join(placeholderPath, file);
-    const fileContents = fs.readFileSync(filePath, "utf8");
-    // Use gray-matter to parse the post metadata section
-    const { data, content } = matter(fileContents);
-
-    const slug = data.title
-      .toLowerCase()
-      .replaceAll(" ", "-")
-      .replaceAll(/[^a-z0-9-_]/g, "");
-
-    return {
-      slug,
-      title: data.title,
-      heroImage: data.heroimage,
-      content: content,
-    };
-  });
-  return allPostsData;
+type BlogDataMap = {
+  didFetch: boolean;
+  posts: BlogPost[];
 }
-// TODO: Super unoptimal, replace this with a more considered data organization
+
+const blogData: BlogDataMap = {
+  didFetch: false,
+  posts: []
+}
+
+export default function getAllPostsData(): BlogPost[] {
+  if (!blogData.didFetch) {
+    console.log("Retrieving blog posts from the disk.")
+    // Get file names under /posts
+    const placeholderPath = path.join(root, "public", "placeholders", "posts");
+    const fileNames = fs.readdirSync(placeholderPath);
+
+    blogData.posts = fileNames.map((file) => {
+      const filePath = path.join(placeholderPath, file);
+      const fileContents = fs.readFileSync(filePath, "utf8");
+      // Use gray-matter to parse the post metadata section
+      const { data, content } = matter(fileContents);
+
+      const slug = data.title
+        .toLowerCase()
+        .replaceAll(" ", "-")
+        .replaceAll(/[^a-z0-9-_]/g, "");
+
+      return {
+        slug,
+        title: data.title,
+        heroImage: data.heroimage,
+        content: content,
+      };
+    });
+
+    blogData.didFetch = true;
+  }
+
+  return blogData.posts;
+}
+
 export function getBlogDataBySlug(slug: string) {
   const bp: BlogPost[] = getAllPostsData();
   return bp.find((blog) => blog.slug === slug);
