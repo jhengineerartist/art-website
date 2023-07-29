c4950440eb56ede9cd16d997e5dd31b62dafb57d
Joseph Hirshman <josehirshman@jhengineerartist.com>
2023-07-28 21:23:04 -0700

    Applied the useref hook so the fullscreen works

--- src/app/posts/[slug]/components/FullScreenImage.tsx
+++ src/app/posts/[slug]/components/FullScreenImage.tsx
@@ -14,28 +14,28 @@ type Params = {
 function FullScreenImage(params: Params) {
     const { src, alt, hidden, hideFullScreen: setHidden } = params;
 
-    const closeRef = useRef(null);
+    const closeRef = useRef<HTMLElement>(null);
 
     // Put a listener for when the user clicks somewhere,
     // determine what object was clicked, and if it wasn't the ref object then
     // close the fullscreen image view
-    // useEffect(() => {
-    //     const handleClickOutside = (event: Event) => {
-    //         console.log(`handleClickOutside ${event.target}`);
-    //         assertEventTargetIsNode(event.target);
-    //         if (closeRef.current && closeRef.current.contains(event.target)) {
-    //             setHidden(true)();
-    //         }
-    //     };
-    //     document.addEventListener('click', handleClickOutside, true);
-    //     return () => {
-    //         document.removeEventListener('click', handleClickOutside, true);
-    //     };
-    // }, [closeRef]);
+    useEffect(() => {
+        const handleClickOutside = (event: Event) => {
+            console.log(`handleClickOutside ${event.target}`);
+            assertEventTargetIsNode(event.target);
+            if (closeRef.current && !closeRef.current.contains(event.target)) {
+                setHidden(true)();
+            }
+        };
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, [closeRef]);
 
     const content = !hidden
         ? (
-            <figure className="flex flex-col rounded-md my-auto bg-panettone-100 justify-center fixed z-10 max-w-5xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-2xl h-[80%] w-[80%]">
+            <figure ref={closeRef} className="flex flex-col rounded-md my-auto bg-panettone-100 justify-center fixed z-10 max-w-5xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 drop-shadow-2xl h-[80%] w-[80%]">
                 <FaTimes className="m-4 text-4xl text-lilprince-900 hover:text-lilprince-200" onClick={setHidden(true)} />
                 <div className="h-4/5 relative">
                     <Image
